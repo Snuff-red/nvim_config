b42fdf6cba1e3bdcc9ab8bda6260472fccf31ad9
Snuff-red <Snuff-red1900@outlook.com>
2023-01-14 20:46:01 +0800

    update

--- lua/lsp/ui.lua
+++ lua/lsp/ui.lua
@@ -1,160 +1,194 @@
 local saga = require('lspsaga')
 
-saga.init_lsp_saga({
--- Options with default value
--- "single" | "double" | "rounded" | "bold" | "plus"
-border_style = "single",
---the range of 0 for fully opaque window (disabled) to 100 for fully
---transparent background. Values between 0-30 are typically most useful.
-saga_winblend = 0,
--- when cursor in saga window you config these to move
-move_in_saga = { prev = '<C-p>',next = '<C-n>'},
--- Error, Warn, Info, Hint
--- use emoji like
--- { "🙀", "😿", "😾", "😺" }
--- or
--- { "😡", "😥", "😤", "😐" }
--- and diagnostic_header can be a function type
--- must return a string and when diagnostic_header
--- is function type it will have a param `entry`
--- entry is a table type has these filed
--- { bufnr, code, col, end_col, end_lnum, lnum, message, severity, source }
-diagnostic_header = { " ", " ", " ", "ﴞ " },
--- preview lines of lsp_finder and definition preview
-max_preview_lines = 10,
--- use emoji lightbulb in default
-code_action_icon = "💡",
--- if true can press number to execute the codeaction in codeaction window
-code_action_num_shortcut = true,
--- same as nvim-lightbulb but async
-code_action_lightbulb = {
+saga.setup({
+  -- Options with default value
+  -- "single" | "double" | "rounded" | "bold" | "plus"
+  border_style = "single",
+  --the range of 0 for fully opaque window (disabled) to 100 for fully
+  --transparent background. Values between 0-30 are typically most useful.
+  saga_winblend = 0,
+  -- when cursor in saga window you config these to move
+  move_in_saga = { prev = '<C-p>', next = '<C-n>' },
+  -- Error, Warn, Info, Hint
+  -- use emoji like
+  -- { "🙀", "😿", "😾", "😺" }
+  -- or
+  -- { "😡", "😥", "😤", "😐" }
+  -- and diagnostic_header can be a function type
+  -- must return a string and when diagnostic_header
+  -- is function type it will have a param `entry`
+  -- entry is a table type has these filed
+  -- { bufnr, code, col, end_col, end_lnum, lnum, message, severity, source }
+  diagnostic_header = { " ", " ", " ", "ﴞ " },
+  -- preview lines of lsp_finder and definition preview
+  max_preview_lines = 10,
+  -- use emoji lightbulb in default
+  code_action_icon = "💡",
+  -- if true can press number to execute the codeaction in codeaction window
+  code_action_num_shortcut = true,
+  -- same as nvim-lightbulb but async
+  lightbulb = {
     enable = true,
     enable_in_insert = true,
-    cache_code_action = true,
     sign = true,
-    update_time = 150,
-    sign_priority = 20,
+    sign_priority = 40,
     virtual_text = true,
-},
--- finder icons
-finder_icons = {
-  def = '  ',
-  ref = '諭 ',
-  link = '  ',
-},
--- finder do lsp request timeout
--- if your project big enough or your server very slow
--- you may need to increase this value
-finder_request_timeout = 1500,
-finder_action_keys = {
-    open = {'o', '<CR>'},
+  },
+  finder = {
+    edit = { 'o', '<CR>' },
     vsplit = 's',
     split = 'i',
     tabe = 't',
-    quit = {'q', '<ESC>'},
-},
-code_action_keys = {
+    quit = { 'q', '<ESC>' },
+  },
+
+  code_action = {
+    num_shortcut = true,
+    keys = {
+      quit = 'q',
+      exec = '<CR>',
+    },
+  },
+
+    definition = {
+    edit = '<C-c>o',
+    vsplit = '<C-c>v',
+    split = '<C-c>i',
+    tabe = '<C-c>t',
     quit = 'q',
+    close = '<Esc>',
+  },
+
+  rename = {
+    quit = '<C-c>',
     exec = '<CR>',
-},
-definition_action_keys = {
-  edit = '<C-c>o',
-  vsplit = '<C-c>v',
-  split = '<C-c>i',
-  tabe = '<C-c>t',
-  quit = 'q',
-},
-rename_action_quit = '<C-c>',
-rename_in_select = true,
--- show symbols in winbar must nightly
--- in_custom mean use lspsaga api to get symbols
--- and set it to your custom winbar or some winbar plugins.
--- if in_cusomt = true you must set in_enable to false
-symbol_in_winbar = {
-    in_custom = false,
+    in_select = true,
+  },
+
+  -- show symbols in winbar must nightly
+  -- in_custom mean use lspsaga api to get symbols
+  -- and set it to your custom winbar or some winbar plugins.
+  -- if in_cusomt = true you must set in_enable to false
+  symbol_in_winbar = {
     enable = true,
     separator = ' ',
+    hide_keyword = true,
     show_file = true,
-    -- define how to customize filename, eg: %:., %
-    -- if not set, use default value `%:t`
-    -- more information see `vim.fn.expand` or `expand`
-    -- ## only valid after set `show_file = true`
-    file_formatter = "",
-    click_support = false,
-},
--- show outline
-show_outline = {
-  win_position = 'right',
-  --set special filetype win that outline window split.like NvimTree neotree
-  -- defx, db_ui
-  win_with = '',
-  win_width = 30,
-  auto_enter = true,
-  auto_preview = true,
-  virt_text = '┃',
-  jump_key = 'o',
-  -- auto refresh when change buffer
-  auto_refresh = true,
-},
--- custom lsp kind
--- usage { Field = 'color code'} or {Field = {your icon, your color code}}
-custom_kind = {},
--- if you don't use nvim-lspconfig you must pass your server name and
--- the related filetypes into this table
--- like server_filetype_map = { metals = { "sbt", "scala" } }
-server_filetype_map = {},
+    folder_level = 2,
+  },
+
+  outline = {
+    win_position = 'right',
+    win_with = '',
+    win_width = 30,
+    show_detail = true,
+    auto_preview = true,
+    auto_refresh = true,
+    auto_close = true,
+    custom_sort = nil,
+    keys = {
+      jump = 'o',
+      expand_collaspe = 'u',
+      quit = 'q',
+    },
+  },
+
+  callhierarchy = {
+    show_detail = false,
+    keys = {
+      edit = 'e',
+      vsplit = 's',
+      split = 'i',
+      tabe = 't',
+      jump = 'o',
+      quit = 'q',
+      expand_collaspe = 'u',
+    },
+  },
+
+    ui = {
+    -- currently only round theme
+    theme = 'round',
+    -- border type can be single,double,rounded,solid,shadow.
+    border = 'solid',
+    winblend = 0,
+    expand = '',
+    collaspe = '',
+    preview = ' ',
+    code_action = '💡',
+    diagnostic = '🐞',
+    incoming = ' ',
+    outgoing = ' ',
+    colors = {
+      --float window normal bakcground color
+      normal_bg = '#1d1536',
+      --title background color
+      title_bg = '#afd700',
+      red = '#e95678',
+      magenta = '#b33076',
+      orange = '#FF8700',
+      yellow = '#f7bb3b',
+      green = '#afd700',
+      cyan = '#36d0e0',
+      blue = '#61afef',
+      purple = '#CBA6F7',
+      white = '#d1d4cf',
+      black = '#1c1c19',
+    },
+    kind = {},
+  },
 })
 
 local lspkind = require('lspkind')
 
 lspkind.init({
-    -- DEPRECATED (use mode instead): enables text annotations
-    --
-    -- default: true
-    -- with_text = true,
+  -- DEPRECATED (use mode instead): enables text annotations
+  --
+  -- default: true
+  -- with_text = true,
 
-    -- defines how annotations are shown
-    -- default: symbol
-    -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
-    mode = 'symbol_text',
+  -- defines how annotations are shown
+  -- default: symbol
+  -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+  mode = 'symbol_text',
 
-    -- default symbol map
-    -- can be either 'default' (requires nerd-fonts font) or
-    -- 'codicons' for codicon preset (requires vscode-codicons font)
-    --
-    -- default: 'default'
-    preset = 'codicons',
+  -- default symbol map
+  -- can be either 'default' (requires nerd-fonts font) or
+  -- 'codicons' for codicon preset (requires vscode-codicons font)
+  --
+  -- default: 'default'
+  preset = 'codicons',
 
-    -- override preset symbols
-    --
-    -- default: {}
-    symbol_map = {
-      Text = "",
-      Method = "",
-      Function = "",
-      Constructor = "",
-      Field = "ﰠ",
-      Variable = "",
-      Class = "ﴯ",
-      Interface = "",
-      Module = "",
-      Property = "ﰠ",
-      Unit = "塞",
-      Value = "",
-      Enum = "",
-      Keyword = "",
-      Snippet = "",
-      Color = "",
-      File = "",
-      Reference = "",
-      Folder = "",
-      EnumMember = "",
-      Constant = "",
-      Struct = "פּ",
-      Event = "",
-      Operator = "",
-      TypeParameter = ""
-    },
+  -- override preset symbols
+  --
+  -- default: {}
+  symbol_map = {
+    Text = "",
+    Method = "",
+    Function = "",
+    Constructor = "",
+    Field = "ﰠ",
+    Variable = "",
+    Class = "ﴯ",
+    Interface = "",
+    Module = "",
+    Property = "ﰠ",
+    Unit = "塞",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "פּ",
+    Event = "",
+    Operator = "",
+    TypeParameter = ""
+  },
 })
 
 vim.diagnostic.config({
